2cf4d007c49a46710c145aa01f8f0c4b7dd230cc
l2ig <leonard@linters.de>
2021-06-15 22:16:21 +0200

    (feat/tests) Add more tests

--- tests/base.ts
+++ tests/base.ts
@@ -9,8 +9,47 @@ if (!projectId || !projectKey || !databaseName)
 
 const base = new Base({ id: projectId, key: projectKey }, databaseName);
 
+Deno.test("base/clean", async () => {
+  for (const item of (await base.query([])).items) {
+    await base.delete(item.key);
+  }
+});
+
 Deno.test("base/create", async () => {
   if (!(await base.insert({ hello: "world" })).processed) {
     throw new Error("Not processed");
+  }
+});
+
+Deno.test("base/query", async () => {
+  if ((await base.query([])).items.length !== 1) {
+    throw new Error("Wrong query item length");
+  }
+});
+
+Deno.test("base/get", async () => {
+  const testItem = (await base.query([])).items[0];
+  if ((await base.get(testItem.key)).hello !== "world") {
+    throw new Error("Wrong item hello value");
+  }
+});
+
+Deno.test("base/update", async () => {
+  const testItem = (await base.query([])).items[0];
+
+  if ((await base.get(testItem.key)).hello !== "world") {
+    throw new Error("Wrong item hello value");
+  }
+
+  await base.update(testItem.key, { set: { hello: "world2" } });
+  if ((await base.get(testItem.key)).hello !== "world2") {
+    throw new Error("Wrong item hello value after update");
+  }
+});
+
+Deno.test("base/delete", async () => {
+  const testItem = (await base.query([])).items[0];
+  if (!(await base.delete(testItem.key)).key) {
+    throw new Error("Wrong item hello value");
   }
 });
